2908826c5d443b11fe9a7d7fe770171ab3c60811
Jackson Graff <jacksongraff@hotmail.com>
2021-06-17 10:39:34 -0300

    solved

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,14 +1,25 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { ParsedUrlQuery } from 'querystring';
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client';
+import Head from 'next/head';
 
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
+import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
+  uid: string;
   first_publication_date: string | null;
   data: {
     title: string;
+    subtitle: string;
     banner: {
       url: string;
     };
@@ -26,20 +37,110 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+interface Params extends ParsedUrlQuery {
+  slug: string;
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
 
-//   // TODO
-// };
+  return (
+    <>
+      <Head>
+        <title>
+          {router.isFallback ? 'Carregando...' : post.data.title} |
+          SpaceTraveling
+        </title>
+      </Head>
+      <Header />
+      {router.isFallback ? (
+        'Carregando...'
+      ) : (
+        <>
+          <img
+            className={styles.header}
+            src={post.data.banner.url}
+            alt="banner"
+          />
+          <article className={styles.container}>
+            <h1>{post.data.title}</h1>
+            <div className={commonStyles.info}>
+              <FiCalendar />
+              <time>
+                {format(new Date(post.first_publication_date), 'd MMM y', {
+                  locale: ptBR,
+                })}
+              </time>
+              <FiUser />
+              <p>{post.data.author}</p>
+              <FiClock />
+              <p>4 min</p>
+            </div>
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+            {post.data.content.map(content => {
+              return (
+                <div className={styles.content} key={content.heading}>
+                  <h2>{content.heading}</h2>
+                  <div
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{
+                      __html: RichText.asHtml(content.body),
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </article>
+        </>
+      )}
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const { results } = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.uid'],
+      pageSize: 1,
+    }
+  );
+
+  return {
+    paths: results.map(result => ({
+      params: {
+        slug: result.uid,
+      },
+    })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as Params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post: Post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content,
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+    redirect: 60 * 30, // 30 minutes
+  };
+};
